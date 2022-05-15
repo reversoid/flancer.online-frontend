f4833f45910c7d9a259357158650ef8b33a1df6f
George <ancient.gekob@gmail.com>
2022-05-15 23:06:37 +0300

    herokuInitial

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -2,6 +2,6 @@ import 'pages/ProfilePage/styles.scss'
 
 export default function ProfilePage() {
   return (
-    <div className='color-light'>ProfilePage</div>
+    <div className='color-light'>ProfilePage!</div>
   )
 }
