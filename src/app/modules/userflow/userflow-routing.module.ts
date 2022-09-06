9102e67245c95278e49e5a9c97c74bade5499ede
George <ancient.gekob@gmail.com>
2022-09-06 16:17:39 +0300

    micro fixes

--- src/app/modules/userflow/userflow-routing.module.ts
+++ src/app/modules/userflow/userflow-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
         path: 'messages',
         loadChildren: () => import('src/app/modules/messages/messages.module').then((m) => m.MessagesModule),
       },
+      {
+        path: '**',
+        redirectTo: 'orders',
+      },
     ],
   },
 ];
