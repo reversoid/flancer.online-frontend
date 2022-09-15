1c247c8a63969fd2de7d1ada76670f87ce8f7360
George <ancient.gekob@gmail.com>
2022-09-15 13:11:28 +0300

    implemented the urls where footer should be shown, created place for future messages

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,4 +1,12 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
+
+const urlWhereShowFooter: RegExp[] = [
+  /messages$/,
+  /profile/,
+  /\/$/,
+];
 
 @Component({
   selector: 'app-footer',
@@ -6,4 +14,35 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./footer.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FooterComponent {}
+export class FooterComponent implements OnInit, OnDestroy {
+  constructor(private router: Router, private changeDetector: ChangeDetectorRef) {}
+
+  public show = false;
+
+  private unsubscribe = new Subject<true>();
+
+  ngOnInit(): void {
+    this.router.events.pipe(takeUntil(this.unsubscribe)).subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const newValue = urlWhereShowFooter.some((url) => url.test(event.url));
+        console.log(event.url, newValue);
+        
+        if (this.show !== newValue) {
+          this.show = newValue; 
+          this.changeDetector.markForCheck();
+        } 
+      }
+    });
+  }
+
+  test() {
+    console.log('rerender footer');
+    return '';
+    
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe.next(true);
+    this.unsubscribe.complete();
+  }
+}
